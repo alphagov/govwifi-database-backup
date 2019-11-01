6a899668d9cc56576d679f0d65bbfd5dd01b6028
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2019-11-01 17:09:56 +0000

    Add mysql dockerfile with test database

---
+++ mysql/schema.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Database: govwifi_test
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP TABLE IF EXISTS `sessions`;
+
+CREATE TABLE `sessions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `start` timestamp NULL DEFAULT NULL,
+  `stop` timestamp NULL DEFAULT NULL,
+  `siteIP` char(15) DEFAULT NULL,
+  `username` char(6) DEFAULT NULL,
+  `cert_name` varchar(64) DEFAULT NULL,
+  `mac` char(17) DEFAULT NULL,
+  `ap` char(17) DEFAULT NULL,
+  `building_identifier` varchar(20) DEFAULT NULL,
+  `success` BOOLEAN DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `siteIP` (`siteIP`,`username`),
+  KEY `sessions_username` (`username`),
+  KEY `sessions_start_username` (`start`,`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ip_locations` (
+  `ip` varchar(30) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-18 11:33:13sess
