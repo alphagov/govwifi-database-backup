04583aa1c40e90fd31616914a806a84670f6014e
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2019-11-18 15:01:31 +0000

    Modifications to the backup script
    - install bash
    - enable -o pipefile incase any of the piped commands error out
    - disable filename globbing
    - identify which backup has passed or failed in a run

--- govwifi-backup.sh
+++ govwifi-backup.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -eu
+set -euf -o pipefail
 
 echo "Starting backup of databases to S3..."
 
@@ -13,7 +13,7 @@ MYSQL_PWD="${WIFI_DB_PASS}" mysqldump \
   | gzip -c | aws ${ENDPOINT_ARG} s3 cp - s3://"${S3_BUCKET}/wifi-backup-$(date -I)".sql.gz
 
 STATUS1=$?
-if [ $STATUS1 -eq 0 ] ; then echo OK ; else echo FAILED ; fi
+if [ $STATUS1 -eq 0 ] ; then echo "OK - Wifi Database" ; else echo "FAILED - Wifi Database"; fi
 
 MYSQL_PWD="${USERS_DB_PASS}" mysqldump \
   -h "${USERS_DB_HOST}" -u "${USERS_DB_USER}" \
@@ -22,7 +22,7 @@ MYSQL_PWD="${USERS_DB_PASS}" mysqldump \
   | gzip -c | aws ${ENDPOINT_ARG} s3 cp - s3://"${S3_BUCKET}/wifi-backup-user-details-$(date -I)".sql.gz
 
 STATUS2=$?
-if [ $STATUS2 -eq 0 ] ; then echo OK ; else echo FAILED ; fi
+if [ $STATUS2 -eq 0 ] ; then echo "OK - Users Databse"; else echo "FAILED - Users Database" ; fi
 
 MYSQL_PWD="${ADMIN_DB_PASS}" mysqldump \
   -h "${ADMIN_DB_HOST}" -u "${ADMIN_DB_USER}" \
@@ -31,7 +31,7 @@ MYSQL_PWD="${ADMIN_DB_PASS}" mysqldump \
   | gzip -c | aws ${ENDPOINT_ARG} s3 cp - s3://"${S3_BUCKET}/wifi-backup-admin-$(date -I)".sql.gz
 
 STATUS3=$?
-if [ $STATUS3 -eq 0 ] ; then echo OK ; else echo FAILED ; fi
+if [ $STATUS3 -eq 0 ] ; then echo "OK - Admin Database"; else echo "FAILED - Admin Database"; fi
 
 if [ $STATUS1 -ne 0 ] || [ $STATUS2 -ne 0 ] || [ $STATUS3 -ne 0 ]
 then
